8771354c5b33620026be6a48b9961edf78a80c63
Leo June Bedeo <leojune.bedeo@pueblodepanay.com>
2021-07-09 10:51:59 +0800

    Add vue components

---
+++ resources/js/Pages/Shop/Products.vue
@@ -0,0 +1,22 @@
+<template>
+  <app-layout>
+    <item />
+  </app-layout>
+</template>
+
+<script>
+import Item from "@/Components/Item";
+import ShowItem from "@/Components/ShowItem";
+import AppLayout from "@/Layouts/AppLayout";
+
+export default {
+  components: {
+    Item,
+    ShowItem,
+    AppLayout,
+  },
+};
+</script>
+
+<style>
+</style>
--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,7 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->name('dashboard');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/items', function () {
+    return Inertia::render('Shop/Products');
+})->name('dashboard');
