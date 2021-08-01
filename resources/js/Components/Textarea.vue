a7a1e97c8e35adacc6678c2b6bee89468d1dea5e
Leo June Bedeo <leojune.bedeo@pueblodepanay.com>
2021-08-02 00:45:34 +0800

    Create textarea vue component

---
+++ resources/js/Components/Textarea.vue
@@ -0,0 +1,17 @@
+<template>
+    <textarea class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"  :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" ref="textarea" />
+</template>
+
+<script>
+    export default {
+        props: ['modelValue'],
+
+        emits: ['update:modelValue'],
+
+        methods: {
+            focus() {
+                this.$refs.textarea.focus()
+            }
+        }
+    }
+</script>
