1af2e75b09f52a6355d560305c07d02819226d3f
Leo June Bedeo <leojune.bedeo@pueblodepanay.com>
2021-08-03 13:51:14 +0800

    Update header text

--- resources/js/Pages/Shop/FormShop.vue
+++ resources/js/Pages/Shop/FormShop.vue
@@ -1,7 +1,7 @@
 <template>
     <app-layout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Shop</h2>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">My Shop</h2>
         </template>
 
         <div class="py-12">
