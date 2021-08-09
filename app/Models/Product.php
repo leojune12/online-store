0570052f6891e738c7180862612265aa1903f517
Leo June Bedeo <leojune.bedeo@pueblodepanay.com>
2021-08-09 15:05:27 +0800

    Add implements HasMedia interface on Product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,12 +5,15 @@ namespace App\Models;
 use App\Models\Shop;
 use App\Models\Order;
 use App\Models\Category;
+use Spatie\MediaLibrary\HasMedia;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\InteractsWithMedia;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Product extends Model
+class Product extends Model implements HasMedia
 {
     use HasFactory;
+    use InteractsWithMedia;
 
     protected $guarded = [];
 
